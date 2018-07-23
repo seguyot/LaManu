d45a4942a8a88f027c0451936c47fcac28085b97
segu <celine.guyot@et.simplon-roanne.com>
2018-07-23 14:41:25 +0200

    hi

---
+++ index.php
@@ -0,0 +1,53 @@
+<?php
+//ex 1
+$name = 'Name';  
+echo $name;
+//ex2
+$lastname = 'Guyot';  
+$firstname = "Celine";    
+$age = 43; 
+
+
+echo " ".$lastname." " . $firstname . " a ". $age.'<br/>';
+//ex3
+$km = 1;
+echo $km;
+$km = 3;
+echo $km;
+$km = 125;
+echo $km; 
+
+//4 string
+$name = "my name is Celine".'<br/>';
+
+//int
+$number = 22;
+
+//float
+$number2 = 22.56;
+
+//booleans
+$sun = true;
+
+//rienex5
+$rien = NULL;
+echo $rien.'<br/>';
+$rien = 32;
+echo $rien.'<br/>';
+//ex6
+$name = 'Name';  
+echo "bonjour " . $name ." comment vas tu ?<br/>";
+//ex7
+$lastname = 'Guyot';  
+$firstname = "Celine";    
+$age = 43; 
+echo  "bonjour " . $lastname . " " . $firstname . " tu as ".$age . " ans<br/>";
+//ex8
+$addition = 3 + 4;
+$multi = 5* 20;
+$divi = 45 / 5;
+echo $addition. '<br/>'. $multi.'<br/>' .$divi;
+
+
+
+
